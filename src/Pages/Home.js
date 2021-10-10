53cf640390dc7116a6bbfcf3836e0fe39532fa41
Cypherspot <awolesiboluwatife@gmail.com>
2021-10-10 08:29:53 +0100

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+    return (
+        <div>
+            <div className="navigation">
+                <Navbar />
+            </div>
+        </div>
+    )
+}
